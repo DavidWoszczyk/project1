50d16cc4335ac8f59ca6e1906a91410b2c12b552
DavidWoszczyk <wdavid.contact@gmail.com>
2026-03-01 14:43:26 +0100

    Remove duplicate create_all

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from .database import engine, Base
 from .routers import users, ai
-from . import models
-Base.metadata.create_all(bind=engine)
-app = FastAPI(title="AI Backend Pro")
 
 Base.metadata.create_all(bind=engine)
+app = FastAPI(title="AI Backend Pro")
 
 app.include_router(users.router)
 app.include_router(ai.router)
